976343cf7d27ea722559d7b6ff0d0de9636042f6
renuneeraja <2300032833@kluniversity.in>
2025-07-21 10:52:28 +0530

    Normalize line endings to LF

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+﻿import ReactLogo from "./ReactLogo.svg?react"
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 })
+import svgr from 'vite-plugin-svgr';
+plugins: [
+  react(),
+  svgr(), // enables React SVG imports
+]
+
